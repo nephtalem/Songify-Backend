d4e0971b8895674d1907a4fd706a4e7273a8d856
nattygirma <nattygirma28@gmail.com>
2024-05-04 23:01:23 -0500

    first commit

--- models/XToken.js
+++ models/XToken.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
-const Schema = mongoose.Schema;
-// Define schema for tokens
 const XtokenSchema = new mongoose.Schema({
     codeVerifier: String,
     state: String,
     accessToken: String,
     refreshToken: String,
-    userId: { type: Schema.Types.ObjectId, ref: 'User' },
+    userId: string,
 },
 {
     timestamps: true,
