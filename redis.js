b343c45c99c2e99504902890ff7c5e3f6123d19c
nattygirma <nattygirma28@gmail.com>
2024-05-30 15:28:31 -0500

    Procfile

--- redis.js
+++ redis.js
@@ -1,7 +1,7 @@
 import Queue from 'bull';
 import "dotenv/config";
 // process.env.REDIS_URL || 
-const REDIS_URL =  || 'redis://127.0.0.1:6379';
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 export const scrapeQueue = new Queue('scrapQueue', REDIS_URL);
 
 
